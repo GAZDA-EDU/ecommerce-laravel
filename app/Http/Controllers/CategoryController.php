a3813a739a008a6cd96d029298cf6520ecb03a2c
GAZDA-EDU <gazda.edu@gmail.com>
2024-11-16 12:16:48 +0100

    creating the Brands and Categories -2 and creating the Products and Locations -1

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categories;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Categories::paginate(10);
+        return response()->json($categories,200);
+    }
+
+    public function show($id)
+    {
+        $category = Categories::find($id);
+        if($category){
+            return response()->json($category, 200);
+        }
+        else
+            return response()->json('category not found');
+    }
+
+    public function store(Request $request)
+    {
+        try{
+            $validated = $request->validate([
+                'name'=>'required|unique:categories,name'
+            ]);
+            $category = new Categories();
+            $category -> name = $request->name;
+            $category -> save();
+            return response()->json('category added', 201);
+        }
+        catch(Exception $e){
+            return response()->json($e, 500);
+        }
+    }
+
+    public function update_category($id, Request $request)
+    {
+        try{
+            $validated = $request->validate([
+                'name' => 'required|unique:categories,name',
+                'image' => 'required'
+            ]);
+            $category = Categories::find($id);
+            if ($request->hasFile('image')){
+                $path = 'assets/uploads/category/' . $category->image;
+                if (File::exists($path)) {
+                    File::delete($path);
+                }
+                $file = $request->file('image');
+                $ext = $file->getClientOriginalExtension();
+                $filename = time() . '.' . $ext;
+                try{
+                    $file->move('assets/uploads/category/', $filename);
+                }
+                catch (FileException $e){
+                    dd($e);
+                }
+                $category->image = $filename;
+            }
+            $category->name = $request->name;
+            $category->update();
+            return response()->json('category updated', 200);
+        }
+        catch(Exception $e){
+            return response()->json($e, 500);
+        }
+    }
+
+    public function delete_category($id)
+    {
+        $category = Categories::find($id);
+        if($category){
+            $category->delete();
+            return response()->json('category deleted');
+        }
+        else
+            return response()->json('category not found');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BrandsController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,8 +25,20 @@ Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
     Route::get('/user-profile', [AuthController::class, 'userProfile']);
 });
 
-Route::get('index', [BrandsController::class, 'index']);
-Route::get('show/{id}', [BrandsController::class, 'show']);
-Route::post('store', [BrandsController::class, 'store']);
-Route::put('update_brand/{id}', [BrandsController::class, 'update_brand']);
-Route::delete('delete_brand/{id}', [BrandsController::class, 'delete']);
+// Brands CRUD
+Route::controller(BrandsController::class)->group(function(){
+    Route::get('index', 'index');
+    Route::get('show/{id}', 'show');
+    Route::post('store', 'store');
+    Route::put('update_brand/{id}', 'update_brand');
+    Route::delete('delete_brand/{id}', 'delete_brand');
+});
+
+// Category CRUD
+Route::controller(CategoryController::class)->group(function(){
+    Route::get('index', 'index');
+    Route::get('show/{id}', 'show');
+    Route::post('store', 'store');
+    Route::put('update_category/{id}', 'update_category');
+    Route::delete('delete_category/{id}', 'delete_category');
+});
